4163accb26313bcaa8e411daedaf781871d366ea
Ahmet <ahmetundemir@outlook.com>
2019-04-14 21:37:36 +0300

    Add project files.

---
+++ Service/Controllers/ServiceController.cs
@@ -0,0 +1,38 @@
+﻿using DAL.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL;
+
+namespace Service.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        // GET: api/Service
+        public List<Product> Get()
+        {
+            using (ShopContext context = new ShopContext())
+            {
+                return null;
+            }
+
+            
+        }
+
+        // GET: api/Service/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Service
+        public void Post([FromBody]string value)
+        {
+        }
+
+       
+    }
+}
